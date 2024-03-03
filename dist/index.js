901ef624fe4c197266d9312a42ea74ec9e1af1fc
Thet Wai Aung <twa453354@gmail.com>
2024-03-03 19:30:15 +0630

    first

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let age = 30;
+let firstName = 'Thet Wai';
+let isExist;
+console.log('Age is' + age);
+console.log('fist ', firstName);
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let age : number =30;
+let firstName : string ='Thet Wai'
+let isExist : boolean 
+
+console.log('Age is'+age);  
+console.log('fist ',firstName)
